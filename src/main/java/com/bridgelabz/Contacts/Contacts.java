83316a71d6ff427613a9353126884b4388fa82fc
mdyaseen95 <yaseen.koppal@gmail.com>
2021-09-26 14:51:40 +0530

    Adding to Commit

---
+++ src/main/java/com/bridgelabz/Contacts/Contacts.java
@@ -0,0 +1,68 @@
+package com.bridgelabz.Contacts;
+
+public class Contacts {
+    private String fname;
+    private String lname;
+    private String address;
+    private String city;
+    private String state;
+    private String zipcode;
+    private String phonenum;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public String getPhonenum() {
+        return phonenum;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public void setPhonenum(String phonenum) {
+        this.phonenum = phonenum;
+    }
+}
+
